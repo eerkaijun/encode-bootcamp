e4be5376250d9bb553102cc62f21283dfc3e448c
Kai Jun Eer <kaijuneer@gmail.com>
2021-11-18 19:21:02 +0000

    finish implementation

--- week1/VolcanoCoin.sol
+++ week1/VolcanoCoin.sol
@@ -6,6 +6,13 @@ contract VolcanoCoin {
     
     uint64 totalSupply = 10000;
     address owner;
+    mapping(address => uint) public balances;
+    mapping(address => Payment[]) public transfers;
+    
+    struct Payment {
+        uint64 amount;
+        address receiver;
+    }
     
     constructor() {
         owner = msg.sender;
@@ -25,5 +32,14 @@ contract VolcanoCoin {
         emit supplyIncreased(totalSupply);
     }
     
+    function transfer(uint64 _amount, address _receiver) public {
+        require(balances[msg.sender] >= _amount);
+        transfers[msg.sender].push(Payment(_amount, _receiver));
+        balances[msg.sender] -= _amount;
+        balances[_receiver] += _amount;
+        emit tokensTransferred(_amount, _receiver);
+    }
+    
     event supplyIncreased(uint64 _newSupply);
+    event tokensTransferred(uint64 _amount, address _receiver);
 }
