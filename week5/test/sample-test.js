55904e137797a557330912458de6a69cc74f65e1
Kai Jun Eer <kaijuneer@gmail.com>
2022-02-19 20:05:35 +0000

    enable UUPS plugin

--- week5/test/sample-test.js
+++ week5/test/sample-test.js
@@ -1,11 +1,13 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 describe("VolcanoCoin", function () {
   it("Should return version number", async function () {
     const Coin = await ethers.getContractFactory("VolcanoCoin");
-    const coin = await Coin.deploy();
+    const coin = await upgrades.deployProxy(Coin, { kind: 'uups' });
+    //const coin = await Coin.deploy();
     await coin.deployed();
+    console.log("Volcano coin contract deployed to: ", coin.address);
 
     expect(await coin.version()).to.equal(1);
   });
