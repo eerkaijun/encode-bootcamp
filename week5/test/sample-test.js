03c8fe8fefb9b22ea6f5f1c428ec8f4e7d8c922f
Kai Jun Eer <kaijuneer@gmail.com>
2022-02-19 21:12:09 +0000

    unit test to check variable value

--- week5/test/sample-test.js
+++ week5/test/sample-test.js
@@ -2,20 +2,43 @@ const { expect } = require("chai");
 const { ethers, upgrades } = require("hardhat");
 
 describe("VolcanoCoin", function () {
-  it("Should return version number correctly after upgrade", async function () {
-    const Coin = await ethers.getContractFactory("VolcanoCoin");
-    const coin = await upgrades.deployProxy(Coin, { kind: 'uups' });
-    //const coin = await Coin.deploy();
-    await coin.deployed();
-    console.log("Volcano coin contract deployed to: ", coin.address);
 
+  let coin;
+  let owner, user;
+
+  beforeEach(async function() {
+
+    [owner, user] = await ethers.getSigners();
+    //console.log("Owner address: ", owner.address);
+    //console.log("User address: ", user.address);
+
+    const Coin = await ethers.getContractFactory("VolcanoCoin");
+    coin = await upgrades.deployProxy(Coin, { kind: 'uups' });
+    await coin.deployed();
+    //console.log("Volcano coin contract deployed to: ", coin.address);
+  });
+
+  it("Should return version number correctly after upgrade", async function () {
     expect(await coin.version()).to.equal(1);
 
     const CoinNew = await ethers.getContractFactory("VolcanoCoinV2");
     const coinNew = await upgrades.upgradeProxy(coin.address, CoinNew);
     await coinNew.deployed();
-    console.log("New Volcano coin contract deployed to: ", coinNew.address);
+    //console.log("New Volcano coin contract deployed to: ", coinNew.address);
 
     expect(await coinNew.version()).to.equal(2);
   });
+
+  it("Should keep the original's contract variable value", async function() {
+    await coin.connect(owner).transfer(user.address, 10);
+    const result = await coin.balanceOf(user.address);
+    //const result = await coin.transfers(owner.address, 0);
+    //console.log("Transfer result: ", result); 
+
+    const CoinNew = await ethers.getContractFactory("VolcanoCoinV2");
+    const coinNew = await upgrades.upgradeProxy(coin.address, CoinNew);
+    await coinNew.deployed();
+
+    expect(await coinNew.balanceOf(user.address)).to.equal(10);
+  })
 });
